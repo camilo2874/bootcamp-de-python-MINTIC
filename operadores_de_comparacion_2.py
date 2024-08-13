12c73e941e3d8c786f81ab3e5488d7c9d59cee34
camilo velasquez <camilovelasquez@gmail.com>
2024-08-12 20:40:19 -0500

    abordamos operadores de comparacion

---
+++ operadores_de_comparacion_2.py
@@ -0,0 +1,35 @@
+es_igual_que =5==5
+print(es_igual_que)
+
+es_diferente_que =5!=6
+print(es_diferente_que)
+
+es_mayor_que =5>5
+print(es_mayor_que)
+
+es_mayor_igual_que =5>=6
+print(es_mayor_igual_que)
+
+es_menor_que =5<6
+print(es_menor_que)
+
+es_menor_igual_que =5<=6
+print(es_menor_igual_que)
+
+#calculos combinados
+
+a = 5
+b= 10
+c= 20
+
+comparacion = a + b == c
+print(comparacion)
+
+#comparar usuarios
+
+contrasena_almacenada ="123456"
+contrasena_ingresada ="123456"
+print(contrasena_almacenada == contrasena_ingresada)
+
+contrasena_almacenada ="123456"
+contrasena_ingresada ="654321"
